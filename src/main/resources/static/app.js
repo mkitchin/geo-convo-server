5f24500396430056d696b46adc6b8ada106e7f37
Michael J. Kitchin <mcoyote@mcoyote.com>
2017-05-16 16:46:12 -0600

    Updated opacity/brightness time ranges.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -95,7 +95,7 @@ let vectorSourceCache = new Map();
 let vectorLayerCache = new Map();
 let newFeatures = [];
 
-let ageThresholdsInMs = [(30 * 60 * 1000), (20 * 60 * 1000), (10 * 60 * 1000), newTimeInMs];
+let ageThresholdsInMs = [(25 * 60 * 1000), (15 * 60 * 1000), (5 * 60 * 1000), newTimeInMs];
 let ageOpacityDeltas = [-0.4, -0.3, -0.2, 0.0];
 let ageBrightnessDeltas = [-70, -50, -30, 0.0];
 
